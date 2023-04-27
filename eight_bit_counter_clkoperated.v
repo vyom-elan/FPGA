4742c50e24b24e53bc6073de22ed0e9106b8abe0
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-04-28 00:12:45 +0530

    Add files via upload

---
+++ eight_bit_counter_clkoperated.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+
+module eight_bit_counter(
+        input clk, reset, output [7:0] count
+    );
+    reg [7:0] counter_up;
+    reg [30:0] clk_counter;
+    always@(posedge clk)
+    begin
+        if(clk_counter == 100000000)
+            clk_counter <=0;
+         else
+            clk_counter <= clk_counter +1'b1;
+     end
+    always@(posedge clk)
+        begin
+            if(reset)
+            counter_up <= 8'b0;
+            else if(clk_counter == 100000000)
+            counter_up <= counter_up +1;
+        end
+        assign count = counter_up;
+endmodule
