afcd2b56522f29f6892b7ff4e615f5d57c83dda8
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-05-01 22:41:08 +0530

    Update eight_bit_counter.v

--- 8bitcounter/eight_bit_counter.v
+++ 8bitcounter/eight_bit_counter.v
@@ -7,7 +7,7 @@ module eight_bit_counter(
     reg [30:0] clk_counter;
     always@(posedge clk)
     begin
-        if(clk_counter == 100000000)
+            if(clk_counter == 50000000)
             clk_counter <=0;
          else
             clk_counter <= clk_counter +1'b1;
@@ -16,7 +16,7 @@ module eight_bit_counter(
         begin
             if(reset)
             counter_up <= 8'b0;
-            else if(clk_counter == 100000000)
+                else if(clk_counter == 50000000)
             counter_up <= counter_up +1;
         end
         assign count = counter_up;
